89b0126d4aa2d49f7db1abed93968b9c285fd519
Mahmoud Elnagar <126767234+Mahmoudelnagar5@users.noreply.github.com>
2025-07-10 12:08:04 +0300

    Create main2.cpp

---
+++ main2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int grade;
+
+    cout << "Enter the score: ";
+    cin >> grade;
+
+    if (grade >= 85) {
+        cout << "Excellent";
+    } else if (grade >= 70) {
+        cout << "Very Good";
+    } else if (grade >= 50) {
+        cout << "Good";
+    } else {
+        cout << "Fail" << endl;
+    }
+    return 0
+}
