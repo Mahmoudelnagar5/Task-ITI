7ec5484e517663d4ca35430b4f7373bfdbdad5e7
Mahmoud Elnagar <126767234+Mahmoudelnagar5@users.noreply.github.com>
2025-07-10 12:09:51 +0300

    Create main3.cpp

---
+++ main3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a = 100;
+  if(a  < 20 ){
+     cout << "a is less than 20" << "\n";
+    }
+    cout << "value of a is " << a << "\n";
+  return 0
+}
